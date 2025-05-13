ed004bf5949bd89f077121f89f1c57a53a16a7d6
June9 <hth1690@naver.com>
2025-05-13 12:50:18 +0900

    ..ㅋ

--- frontend/src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="bg-gray-100 dark:bg-gray-800 py-4 text-center w-full flex-shrink-0">
-        <div className="container mx-auto">
-          <p className="text-gray-600 dark:text-gray-400">Mountain 커뮤니티 © {new Date().getFullYear()}</p>
-        </div>
-      </footer>
-    )
-  }
